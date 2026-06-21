8a8c51af186956ca6f37f2f8ca59449a2719fd81
tihomir <tstanoev@yandex.com>
2026-06-21 22:05:59 +0300

    Add tests for Profile views authentication and permissions

---
+++ tests/master_data/views/test_profile_views.py
@@ -0,0 +1,68 @@
+from decimal import Decimal
+import pytest
+from django.contrib.auth import get_user_model
+from rest_framework.test import APIClient
+from django.urls import reverse
+from model_bakery import baker
+
+
+
+UserModel = get_user_model()
+
+
+@pytest.mark.django_db
+def test_anon_can_list_profiles():
+    client = APIClient()
+
+    response = client.get('/api/master-data/profiles/')
+
+    assert response.status_code == 401
+
+
+@pytest.mark.django_db
+def test_logged_user_can_list_profiles():
+    user = baker.make(
+        UserModel,
+        role=UserModel.Role.USER)
+    client = APIClient()
+    client.force_authenticate(user=user)
+
+    response = client.get('/api/master-data/profiles/')
+
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_moderator_can_create_profile():
+    user = baker.make(
+        UserModel,
+        role=UserModel.Role.MODERATOR)
+    client = APIClient()
+    client.force_authenticate(user=user)
+    payload = {
+        'code': '51233',
+        'description': 'DESCRIPTION',
+        'theoretical_gpm': Decimal('1000.00'),
+    }
+
+    response = client.post('/api/master-data/profiles/', data=payload)
+
+    assert response.status_code == 201
+
+
+@pytest.mark.django_db
+def test_user_cannot_create_profile():
+    user = baker.make(
+        UserModel,
+        role=UserModel.Role.USER)
+    client = APIClient()
+    client.force_authenticate(user=user)
+    payload = {
+        'code': '51233',
+        'description': 'DESCRIPTION',
+        'theoretical_gpm': Decimal('1000.00'),
+    }
+
+    response = client.post('/api/master-data/profiles/', data=payload)
+
+    assert response.status_code == 403
